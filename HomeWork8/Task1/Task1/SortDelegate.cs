fd9976ccc1b452057ac00ef0e55c642984a10b6e
senivlm <91743906+senivlm@users.noreply.github.com>
2021-10-26 11:51:00 +0300

    Update SortDelegate.cs

--- HomeWork8/Task1/Task1/SortDelegate.cs
+++ HomeWork8/Task1/Task1/SortDelegate.cs
@@ -1,14 +1,14 @@
 ﻿namespace Task1
 {
     class SortDelegate
-    {
+    {делегат краще було винести поза клас
         public delegate int CompareObjects(object obj1, object obj2);
         public static int Sort(object[] objArray, CompareObjects compareMethod)
         {
             for (int i = 0; i < objArray.Length; ++i)
             {
                 for (int j = 0; j < objArray.Length - i - 1; ++j)
-                {
+                {Що буде, якщо занести в масив різнотипові об'єкти?
                     if (compareMethod(objArray[j], objArray[j+1]) > 0)
                     {
                         (objArray[j], objArray[j + 1]) = (objArray[j + 1], objArray[j]);
